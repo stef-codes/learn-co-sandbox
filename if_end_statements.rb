f3b909c58366bff5af9d2f123c6beb3bbc02bdc7
Stefanie Davis <davisstefanie2010@gmail.com>
2019-11-04 00:01:34 +0000

    Automatically backed up by Learn

---
+++ if_end_statements.rb
@@ -0,0 +1,37 @@
+=begin
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end" 
+=end 
+=begin
+chance_of_rain = 1
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
+=end 
+
+=begin
+chance_of_rain = -23
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else 
+  puts "Stay at home and read Hegel."
+end
+=end
+
+this_year = 2019
+puts "Hey, it's not 2019!"
+unless
+this_year == 2019
+
+
+
